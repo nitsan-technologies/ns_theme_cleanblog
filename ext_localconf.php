5a842119ff19061d632bb5424315b21a6c233f39
NITSAN <sanjay@nitsan.in>
2020-01-08 17:45:53 +0530

    [TODO] Clean-up old version

--- ext_localconf.php
+++
@@ -1,8 +0,0 @@
-<?php
-// TYPO3 Security Check
-if (!defined('TYPO3_MODE')) {
-	die('Access denied.');
-}
-
-// Let's configuration of this extension from "Extension Manager"
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
