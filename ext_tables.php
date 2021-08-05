fe9bb7c0f159db892664ac2bfccbc77fe87e3aea
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2021-08-05 15:27:14 +0530

    [TODO] Code improvements

--- ext_tables.php
+++ ext_tables.php
@@ -6,4 +6,4 @@ if (!defined('TYPO3_MODE')) {
 
 // Add default include static TypoScript (for root page)
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ns_theme_cleanblog', 'Configuration/TypoScript', '[NITSAN] Clean Blog Theme & Templates');
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_theme_cleanblog/Configuration/PageTSconfig/setup.ts">');
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:ns_theme_cleanblog/Configuration/PageTSconfig/setup.typoscript">');
