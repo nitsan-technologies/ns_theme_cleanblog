52fcb34a458f303560e066c52c16ee4717da7c61
nitsan-technologies <sanjay@nitsan.in>
2021-12-31 19:55:55 +0530

    Theme update and changes

--- ext_emconf.php
+++ ext_emconf.php
@@ -8,29 +8,25 @@
 
 // Provide detailed information and depenencies of EXT:ns_theme_cleanblog
 $EM_CONF['ns_theme_cleanblog'] = array(
-	'title' => '[NITSAN] Clean Blog TYPO3 Template',
-	'description' => 'The clean blog is a premium TYPO3 template for personal or company blog with minimal & clean design, super easy settings and ultra fast page speed. Live-Demo: https://demo.t3terminal.com/?theme=t3t-cleanblog PRO version: https://t3terminal.com/t3-clean-blog-free-typo3-template',
+	'title' => '[NITSAN] T3 Clean Blog | Free TYPO3 Template',
+	'description' => 'T3 Clean Blog Free TYPO3 Template is a premium TYPO3 theme for personal or company blog with minimal & clean design, super easy settings, light, and ultra-fast page speed.',
 	'category' => 'templates',
 	'author' => 'NITSAN Technologies Pvt Ltd',
 	'author_email' => 'info@nitsan.in',
 	'author_company' => 'NITSAN Technologies Pvt Ltd',
 	'state' => 'stable',
-	'version' => '2.1.8',
+	'version' => '3.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'typo3' => '9.0.0-10.99.99',
-			'news' => '8.0.0-10.99.99',
-			'ns_basetheme' => '9.0.0-11.9.99',
-			'gridelements' => '9.0.0-10.9.99',
-			'ns_news_comments' => '1.0.0-10.9.99',
+			'typo3' => '10.0.0-11.5.99',
+			'news' => '9.0.0-9.99.99',
+			'ns_basetheme' => '10.0.0-11.5.99',
+			'ns_news_comments' => '4.0.0-5.5.99',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	//'autoload' => array(
-	//	'classmap' => array('Classes/'),
-	//),
 );
 ?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -1,8 +1,6 @@
 <?php
-// TYPO3 Security Check
-if (!defined('TYPO3_MODE')) {
-    die('Access denied.');
-}
+$_EXTKEY = "ns_theme_cleanblog";
 
 // Let's configuration of this extension from "Extension Manager"
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['ns_theme_cleanblog'] = unserialize($_EXTCONF);
+$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:ns_theme_cleanblog/Configuration/RTE/Default.yaml';
