9c6c7ff420a41649825838de7e28d33aad934669
nitsan <rmahesh.nitsan@gmail.com>
2022-08-10 16:29:08 +0530

    [TASK] Compatible with php version 8.1

--- ext_localconf.php
+++ ext_localconf.php
@@ -2,5 +2,5 @@
 $_EXTKEY = "ns_theme_cleanblog";
 
 // Let's configuration of this extension from "Extension Manager"
-$GLOBALS['TYPO3_CONF_VARS']['EXTCONF'][$_EXTKEY] = unserialize($_EXTCONF);
+
 $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:ns_theme_cleanblog/Configuration/RTE/Default.yaml';
