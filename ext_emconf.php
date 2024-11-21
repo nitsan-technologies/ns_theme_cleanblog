33983e054b1e3cdef37ae7db759ecb36f714c283
Mahesh Rathod <rmahesh.nitsan@gmail.com>
2024-11-21 17:40:01 +0530

    [TASK] TYPO3 v13 compatibility

--- ext_emconf.php
+++ ext_emconf.php
@@ -14,7 +14,7 @@ $EM_CONF['ns_theme_cleanblog'] = array (
   'title' => '[NITSAN] T3 Clean Blog | Free TYPO3 Template',
   'description' => 'T3 Clean Blog Free TYPO3 Template is a premium TYPO3 theme for personal or company blog with minimal & clean design, super easy settings, light, and ultra-fast page speed. Live-Demo: https://demo.t3terminal.com/?theme=t3t-cleanblog PRO version: https://t3planet.com/t3-clean-blog-free-typo3-template',
   'category' => 'templates',
-  'version' => '12.0.2',
+  'version' => '13.0.0',
   'state' => 'stable',
   'uploadfolder' => false,
   'author' => 'Team NITSAN',
@@ -24,10 +24,10 @@ $EM_CONF['ns_theme_cleanblog'] = array (
   array (
     'depends' => 
     array (
-      'typo3' => '12.0.0-12.5.99',
-      'news' => '11.0.0-11.9.99',
-      'ns_basetheme' => '12.0.0-12.5.99',
-      'ns_news_comments' => '12.0.0-12.5.99',
+      'typo3' => '13.0.0-13.5.99',
+      'news' => '12.0.0-12.9.99',
+      'ns_basetheme' => '13.0.0-13.5.99',
+      'ns_news_comments' => '13.0.0-13.5.99',
     ),
     'conflicts' => 
     array (
