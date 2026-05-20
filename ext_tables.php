f45f75688b3cd88a6caf9c895dff91078c223d4d
Saloni Thakkar <saloni.nitsan@gmail.com>
2026-05-20 11:07:44 +0530

    [TASK]siteset to add

--- ext_tables.php
+++ ext_tables.php
@@ -3,4 +3,4 @@
 // Add default include static TypoScript (for root page)
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('ns_theme_cleanblog', 'Configuration/TypoScript', '[NITSAN] T3 Clean Blog | Free TYPO3 Template');
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:ns_theme_cleanblog/Configuration/PageTSconfig/setup.typoscript'");
+// \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig("@import 'EXT:ns_theme_cleanblog/Configuration/PageTSconfig/setup.typoscript'");
